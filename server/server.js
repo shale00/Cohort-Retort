f9086dec5253ac46cf55c625e30757eb3e0f0be9
sth <sth0067@gmail.com>
2023-08-11 21:44:53 -0500

    added build to server, reverted server.js file

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const path = require("path");
-const { authMiddleware } = require("./utils/auth");
-const cors = require('cors');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 
-const { typeDefs, resolvers } = require("./schemas");
-const db = require("./config/connection");
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,44 +14,35 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets from the React app build folder
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-
-  // For any other route, send the React app's HTML file
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-
-}
-app.use(express.static(path.join(__dirname, "build")));
-
+app.use(express.static('build'));
 
 // Serve up static assets
-app.use("/images", express.static(path.join(__dirname, "../client/images")));
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
-
-
-  // Define your API route for Apollo Server
-  server.applyMiddleware({ app, path: "/graphql" });
-
-  db.once("open", () => {
+  
+  db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
-    });
-  });
-};
-
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+  
 // Call the async function to start the server
-startApolloServer();
+  startApolloServer();

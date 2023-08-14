f54d4f903e31ae9eec3c1ae6d1e2db00a1703d54
sth <sth0067@gmail.com>
2023-08-13 21:42:41 -0500

    updated app.get

--- server/server.js
+++ server/server.js
@@ -22,16 +22,16 @@ app.use(express.static(path.join(__dirname,'build')));
 // Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('/api', (req, res) => {
-    res.sendFile(path.join(__dirname, '/build/index.html'));
-  });
-}
+//   app.get('/api', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/build/index.html'));
+//   });
+// }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
 
